e416bbf5093f1faa9b4c99b47274c1f5e1d15ba5
matheusvp2 <matheusvp2@ucl.br>
2020-09-09 02:24:15 +0000

    att

--- main.cs
+++ main.cs
@@ -7,11 +7,13 @@ class MainClass {
     matheus.setNome("Matheus Oliveira");
     matheus.setIdade(10);
     matheus.setPeso(70);
+    matheus.setAltura(1.50);
     matheus.crescer();
 
     Console.WriteLine( matheus.getNome() );
     Console.WriteLine( matheus.getIdade() );
     Console.WriteLine( matheus.getPeso() );
+    Console.WriteLine( matheus.getAltura() );
 
   }
 }
